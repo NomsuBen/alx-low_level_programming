b5e61b5962e1a2863bf01715af81c0201373fde7
NomsuBen <olaniranbenjamin@gmail.com>
2022-09-08 13:48:31 -0700

    Just made a 4-puts.c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
+ */
+int main(void)
+{
+	char Mystr[] = "Programming is like building a multilingual puzzle";
+
+	puts(Mystr);  stdout
+	return (0);
+		}	
