4dd2a9cc06a64447b571dc501938e7c2d8cc2ceb
NomsuBen <olaniranbenjamin@gmail.com>
2022-09-14 13:02:06 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -18,7 +18,7 @@ void print_times_table(int n)
 				if (z > 99)
 				{
 					_putchar(',');
-					_putchar(32)
+					_putchar(32);
 					_putchar((z / 100) + '0');
 					_putchar(((z / 10) % 10) + '0');
 					_putchar((z % 10) + '0');
