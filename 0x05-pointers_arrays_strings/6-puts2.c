4f9432295c4ed61c0709ad54a02d1793ef03c0a6
NomsuBen <olaniranbenjamin@gmail.com>
2022-09-19 13:57:51 -0700

    Just made a 6-puts2.c file

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,11 +1,11 @@
-nclude "main.h"
+#include "main.h"
 
 /**
  * puts2 - prints every other character of a string, starting with the first character, followed by a new line
  * @str: the used string reference
  * Return: 0
  */
-void puts2(char *str);
+void puts2(char *str)
 {
 	int i = o;
 
