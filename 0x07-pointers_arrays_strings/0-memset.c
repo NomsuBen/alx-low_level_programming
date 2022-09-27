f1d607e6ddf61a3fdb13cd9ae1717472d734cbf3
NomsuBen <olaniranbenjamin@gmail.com>
2022-09-27 11:13:35 -0700

    Just made a 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,7 +2,7 @@
 
 /**
  * *_memset - fills memory with a constant byte
- * @s: pointer to put the constant 
+ * @s: pointer to put the constant
  * @b: constant byte
  * @n: max bytes to use
  * Return: s
@@ -12,7 +12,7 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; n > 0; i++,n--)
+	for (i = 0; n > 0; i++, n--)
 	{
 		s[i] = b;
 	}
