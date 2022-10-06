b073be3144c47e1530baf1064672e6e5e7356cfe
NomsuBen <olaniranbenjamin@gmail.com>
2022-10-06 14:01:52 -0700

    Just made a 100-realloc.c file

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -41,6 +41,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	for (i = 0; i < old_size; i++)
 		*((char *)new_ptr + i) = *((char *)ptr + i);
+
 	free(ptr);
 	return ((void *)new_ptr);
-}	
+}
